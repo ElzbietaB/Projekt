d6947abc42cc5064e73782edd0d11983760ae2de
EB <brzelka@o2.pl>
2022-06-28 19:42:28 +0200

    test

--- cw1/script.js
+++ cw1/script.js
@@ -16,30 +16,18 @@ const data = [
 //console.log(data)
 
 function changeHTMLInfo(slideNr){
-    const title = data[slideNr].title
-    console.log(title);
+    const title = data[slideNr].title;
+//    console.log(title);
 
     const titleDOM=document.querySelector("h2")
     titleDOM.innerHTML=title
 
+    const image = data[slideNr].src;
+//    console.log(image);
+
+    const imageDOM=document.querySelector("img")
+    imageDOM.src="img/"+image
+
 }
 
-changeHTMLInfo(2)
-
-
-
-
-
-
-
-
-/*console.log("Hello World");
-
-const a=6;
-const b=7;
-
-if(a<b){
-    console.log("A jest mniejsze")
-} else {
-    console.log("i co ty na to")
-}*/
+changeHTMLInfo(1)
