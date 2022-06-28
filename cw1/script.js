2ae3a3cd264d01ff1c68fe91d7e228272c0fbc76
EB <brzelka@o2.pl>
2022-06-28 19:16:37 +0200

    test

--- cw1/script.js
+++ cw1/script.js
@@ -1,3 +1,31 @@
+const data = [
+{
+    title:"Pink lake",
+    src: "1.webp",
+},
+{
+    title:"Single island",
+    src: "2.webp",
+},
+{
+    title:"Forest lake",
+    src: "3.webp",
+},
+]
+
+//console.log(data)
+
+function changeHTMLInfo(slideNr){
+    const title = data[slideNr].title
+    console.log(title);
+
+    const titleDOM=document.querySelector("h2")
+    titleDOM.innerHTML=title
+
+}
+
+changeHTMLInfo(2)
+
 
 
 
