411ce6750eaaa1124abf0d254df432d5b4da56a9
EB <brzelka@o2.pl>
2022-06-28 21:48:05 +0200

    test

--- cw1/script.js
+++ cw1/script.js
@@ -15,19 +15,48 @@ const data = [
 
 //console.log(data)
 
-function changeHTMLInfo(slideNr){
+function changeHTMLInfo( slideNr ){
+ 
     const title = data[slideNr].title;
 //    console.log(title);
-
     const titleDOM=document.querySelector("h2")
     titleDOM.innerHTML=title
 
     const image = data[slideNr].src;
 //    console.log(image);
-
     const imageDOM=document.querySelector("img")
     imageDOM.src="img/"+image
-
 }
 
 changeHTMLInfo(1)
+
+let currSlide = 1;
+function changeSlide( direction ){
+    if ( direction == "left"){  
+        currSlide--  
+    }else{
+        currSlide++  
+    }
+
+    if (currSlide<0) {
+      currSlide=data.length-1  
+    }
+
+    if (currSlide>data.length-1) {
+        currSlide=0 
+      }
+
+ //   console.log(currSlide)
+    changeHTMLInfo(currSlide)
+}
+changeHTMLInfo(currSlide)
+ 
+const leftArr = document.querySelector(".leftArr")
+leftArr.onclick = function(){
+    changeSlide("left")
+}
+
+const rightArr = document.querySelector(".rightArr")
+rightArr.onclick = function(){
+    changeSlide("right")
+}
