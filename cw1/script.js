ec78a616291927d71131552f78d4f3daf673fd63
EB <brzelka@o2.pl>
2022-06-28 16:51:26 +0200

    test

--- cw1/script.js
+++ cw1/script.js
@@ -1,3 +1,12 @@
+
+
+
+
+
+
+
+/*console.log("Hello World");
+
 const a=6;
 const b=7;
 
@@ -5,4 +14,4 @@ if(a<b){
     console.log("A jest mniejsze")
 } else {
     console.log("i co ty na to")
-}
+}*/
