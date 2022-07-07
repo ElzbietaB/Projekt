390a65ab2b6fd244afd3f94a39bc00baa93c8d4f
EB <brzelka@o2.pl>
2022-07-07 18:40:08 +0200

    test

--- cw2/script.js
+++ cw2/script.js
@@ -1,3 +1,12 @@
-//console.log("Hello")
 const gamePlane=document.querySelector(".gamePlane")
-gamePlane.style.cssText="border:2px dashed red; height:20px;"
+gamePlane.style.cssText=`
+    border:2px 
+    dashed red; 
+    height:20px;
+`
+
+
+const wall=document.createElement("div")
+wall.style.cssText='backgroung-color:red; widht:50px; height;50px; left:100px; top:100px;'
+
+gamePlane.append(wall)
